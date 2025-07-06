20e490a158cbf73170362491a6aff430bd5c03d5
mustaqeem-m <mmmustaqeem1910@gmail.com>
2025-07-06 06:26:25 +0530

    Validation wrapped up

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 - Create react app
 - Configure Tailwind (tailwindcss@3);
+- Routing of login and browse page
 - Header
 - login from
-- Signup form
+- Signup/SignUp form
+- Form validation
+- useRef hook
 
 # Features
 
@@ -24,3 +27,21 @@
 
 - Formik is a library used when we have a form in our react application
 - without formik -> handling validation, errors and other things related to forms becomes very difficult
+
+# useRef hook
+
+- Its a react hook thats lets us reference a value that's not needed for rendering
+
+- useRef is a hook in React that gives us:
+
+- A mutable object that persists across renders
+
+- A way to access DOM elements (like <input>)
+
+- A place to store any value without triggering re-render
+
+<const refVar = useRef(initialValue);>
+
+- refVar.current will hold your value.
+
+- Can be attached to a DOM element using the ref prop.
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,77 +1,134 @@
-import React, { useState } from 'react'
-import Header from './Header'
+import React, { useState,useRef } from 'react'
+import Header from './Header';
+import { checkValidData, ValidateUserName } from '../utils/validate';
 
 const Login = () => {
-  const [isSignInForm,SetIsSignInForm] = useState(true);
+  const [isSignInForm, SetIsSignInForm] = useState(true);
+
+  const email = useRef(null);
+  const password = useRef(null);
+  const userName = useRef(null);
+  const rePassword = useRef(null);
+
+  const [errorMessage,setErrorMessage] = useState(null); 
+
+  const handleFormSubmit = () => {
+    const emailVal =  email.current.value;
+    const passwordVal = password.current.value;
+   
+
+    if(isSignInForm) {
+      const result = checkValidData(emailVal,passwordVal);
+      setErrorMessage(result);
+    }else{
+      const userNameVal = userName.current?.value;
+      const rePasswordValue = rePassword.current?.value;
+
+      const userErr = ValidateUserName(userNameVal);
+      if(userErr) {
+        setErrorMessage(userErr);
+        return;
+      }
+
+      const credErr = checkValidData(emailVal,passwordVal);
+
+      if(credErr) {
+        setErrorMessage(credErr);
+        return;
+      } 
+
+      if(passwordVal !== rePasswordValue){
+        setErrorMessage("Password do not match");
+        return;
+      }
+
+      setErrorMessage(null);
+    }
+
+  }
+
 
   const toggleSignInForm = () => {
     SetIsSignInForm(!isSignInForm);
   };
 
+  
+
   return (
     <div>
-        <Header/> 
-        <div className='absolute'>
-          <img 
-      src='https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg'
-      alt='bg-image'/>
-        </div>
-      
-      <form className="absolute bg-black bg-opacity-80 text-white p-12 w-3/12 left-0 right-0 mx-auto my-40 rounded-md shadow-lg space-y-6">
+      <Header />
+      <div className='absolute'>
+        <img
+          src='https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg'
+          alt='bg-image' />
+      </div>
+
+      <form className="absolute bg-black bg-opacity-80 text-white p-12 w-3/12 left-0 right-0 mx-auto my-40 rounded-md shadow-lg space-y-6"
+      onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="text-3xl font-bold text-center">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm ? <input
+          ref={userName}
           type="text"
           placeholder="User name"
           className="w-full p-3 bg-gray-800 rounded focus:outline-none focus:ring-2 focus:ring-red-700"
         /> : ''}
 
         <input
+        ref = {email}
           type="text"
           placeholder="User name or Email address"
           className="w-full p-3 bg-gray-800 rounded focus:outline-none focus:ring-2 focus:ring-red-700"
-        /> 
+        />
 
         <input
+          ref = {password}
           type="password"
           placeholder="Password"
           className="w-full p-3 bg-gray-800 rounded focus:outline-none focus:ring-2 focus:ring-red-700"
         />
 
         {!isSignInForm ? <input
+          ref={rePassword}
           type="password"
           placeholder="Re-enter password"
           className="w-full p-3 bg-gray-800 rounded focus:outline-none focus:ring-2 focus:ring-red-700"
         /> : ''}
+        <p className='text-red-600'>{errorMessage}</p>
 
-        <button className="w-full bg-red-700 hover:bg-red-800 text-white py-3 rounded font-semibold">
-          {isSignInForm ? "Sign In" : "Sign Up"} 
+        <button className="w-full bg-red-700 hover:bg-red-800 text-white py-3 rounded font-semibold"
+        onClick = {
+            handleFormSubmit
+          } >
+          {isSignInForm ? "Sign In" : "Sign Up"}  
+          
         </button>
 
         {isSignInForm ? <div>
           <h1 className='text-1xl mx-44'>OR</h1>
           <br></br>
 
-        <button className="w-full bg-gray-600 hover:bg-gray-800 text-white py-3 rounded font-semibold">
-          Use a Sign in code
-        </button>
+          <button className="w-full bg-gray-600 hover:bg-gray-800 text-white py-3 rounded font-semibold">
+            Use a Sign in code
+          </button>
         </div> : ''}
 
-        
+
 
         <div className='flex text-white mt'>
           <p>{isSignInForm ? "New to Netflix?" : "Already registered ? "}</p>
           <p className='font-bold cursor-pointer hover:underline '
-          onClick={toggleSignInForm}>
-            {isSignInForm ? "Sign Up Now" : "Sign In now"}  
-            </p>
-        
-          
+            onClick={toggleSignInForm}>
+            {isSignInForm ? "Sign Up Now" : "Sign In now"}
+          </p>
+
+
 
         </div>
 
         {isSignInForm ? <h1 className='underline cursor-pointer'>Forgot Password</h1> : ''}
-        
-        
+
+
       </form>
 
 
